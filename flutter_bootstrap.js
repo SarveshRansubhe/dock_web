edf32f890a3eb9c2c56c7d1c89bbeaeea60bb1ae
sarveshcoditron <sarvesh.ransubhe@coditron.com>
2024-12-25 00:44:42 +0530

    ok

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "456126896"
+    serviceWorkerVersion: "4100880747"
   }
 });
